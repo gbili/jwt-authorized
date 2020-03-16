965537d82b06368c366cc79adc7259f14940d736
gbili <accounts.guillermo@gmail.com>
2020-03-16 12:12:12 +0100

    fix: not exporting correctly

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { loadThroughDi, tokenAuthService } from './loaders';
+import { loadThroughDi, tokenAuthServiceCallable } from './loaders';
 import TokenAuthService from './services/TokenAuthService';
 import HeaderAuthTokenExtractor from './services/HeaderAuthTokenExtractor';
 import { TokenUser } from './models';
 import tokenConfigGenerator from './config/tokenConfigGenerator';
 
-export { TokenAuthService, HeaderAuthTokenExtractor, TokenUser, tokenConfigGenerator, loadThroughDi, tokenAuthService }
+export { TokenAuthService, HeaderAuthTokenExtractor, TokenUser, tokenConfigGenerator, loadThroughDi, tokenAuthServiceCallable }
 export default loadThroughDi;
